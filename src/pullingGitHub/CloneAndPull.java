251f3d403591a9ad57308b70068bb1f20f68000f
azad <you@example.com>
2022-05-12 22:07:28 -0700

    class practice

--- src/pullingGitHub/CloneAndPull.java
+++ src/pullingGitHub/CloneAndPull.java
@@ -1,10 +1,16 @@
 package pullingGitHub;
 
+
 public class CloneAndPull {
 	public static void main(String[] args) {
 		System.out.println("practing pull and merege in GitHub");
 		System.out.println("a+b");
 		
+		
+			System.out.println("GitHub class practice");
+			
+		}
+		
 	}
 
-}
+
